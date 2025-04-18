e4ba0dd26abe9490e4e81b82b65ba9584fd84473
kawasakik1 <granz.meer.3k@gmail.com>
2025-04-18 20:55:33 +0900

    APIを修正

--- constants/api.ts
+++ constants/api.ts
@@ -1,7 +1,7 @@
 // API関連の定数
 export const API = {
   // サーバーサイドでのみ使用する値
-  BASE_URL: process.env.API_URL || "http://localhost:8080",
+  BASE_URL: process.env.API_URL || "https://phraseapp-backend-production-53ba.up.railway.app",
   ENDPOINTS: {
     CATEGORIES: "/category",
     PHRASES: "/phrase",
